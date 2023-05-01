90fa5dd8ced019354f4531d218e8ef96aceb2852
BinhLe <binh.le100799@gmail.com>
2023-05-01 22:58:19 +0700

    add link contact

---
+++ script/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $("#twitter_icon").click(function () {
+        window.open("https://twitter.com/BnhL24458240");
+    });
+    $("#facebook_icon").click(function () {
+        window.open("https://www.facebook.com/binh.le1007");
+    });
+    $("#githunb_icon").click(function () {
+        window.open("https://github.com/BinhLe-FDev");
+    });
+  });
